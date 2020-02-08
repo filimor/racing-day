971fcac449b299cf74514e43a73ea94536dc0e7c
Filipe Moreira <filimor@posteo.com.br>
2020-02-08 08:50:49 -0300

    Release v1.0

--- RacingDay/Properties/AssemblyInfo.cs
+++ RacingDay/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RacingDay")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Racing Day")]
+[assembly: AssemblyDescription("A simple dog racing game that explores basic concepts of C# language and OOP.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RacingDay")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
